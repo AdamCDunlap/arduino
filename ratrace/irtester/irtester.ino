7720174245ab6dc60de0bb85dc8c991f4f50d947
Adam Dunlap <dunlapadam@gmail.com>
2012-06-11 16:18:37 -0700

    Making irtester more accurate

--- ratrace/irtester/irtester.ino
+++ ratrace/irtester/irtester.ino
@@ -1,11 +1,28 @@
+#include <RunningMedian.h>
+
+RunningMedian rightmed;
+RunningMedian leftmed;
+
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-    Serial.print(analogRead(A5));
-    Serial.print('\t');
-    Serial.print(analogRead(A4));
+    rightmed.add(analogRead(A5));
+    analogRead(A4);
+    int rightval = rightmed.getMedian();
+    Serial.print(rightval);
+    Serial.print("\t=");
+    Serial.print(43.89432 - 0.12038 * rightval + 0.0001038 * rightval * rightval);
+    Serial.print("in\t");
+    int leftval = leftmed.getMedian();
+    Serial.print(leftval);
+    Serial.print("\t=");
+    Serial.print(43.89432 - 0.12038 * leftval + 0.0001038 * leftval * leftval);
+    Serial.print("in\t");
     Serial.println();
-    delay(200);
+    delay(100);
+    leftmed.add(analogRead(A4));
+    analogRead(A4);
+    delay(100);
 }
