48278280e3cac6c27fea768a4c97a0331939eced
Adam Dunlap <dunlapadam@gmail.com>
2012-06-22 20:40:52 -0700

    Testing ultrasonic sensor code

--- ultrasonic/test2/test2.ino
+++ ultrasonic/test2/test2.ino
@@ -9,13 +9,14 @@ uint8_t USpin = 4;
 
 void setup();
 void loop();
+void manageUltrasonic(const byte USpin, unsigned int* distance);
 
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-    static unsigned int distance; // in cm
+    static unsigned int distance; // in deciinches
     static unsigned int pulsetime;
 
     unsigned long currentTimeMs = millis();
@@ -24,7 +25,28 @@ void loop() {
 
     unsigned long looptimeUs = currentTimeUs - lastTimeUs;
 
-    // Used for storing a time, depending on state
+    manageUltrasonic(USpin, &distance);
+
+    static unsigned long lastPrintTime = 0;
+    if (millis() - lastPrintTime >= 1000) {
+        //Serial.print("Time: ");   Serial.print(pulsetime); Serial.print('\t');
+        //Serial.print("Distance"); Serial.print(distance);  Serial.print('\t');
+        //DEBUG_PRINT(pulsetime);
+        DEBUG_PRINT(distance);
+        //DEBUG_PRINT(USstate);
+        DEBUG_PRINT(looptimeUs);
+        Serial.println();
+
+        lastPrintTime = millis();
+    }
+
+    lastTimeUs = currentTimeUs;
+}
+
+void manageUltrasonic(const byte USpin, unsigned int* distance) {
+    unsigned long currentTimeUs = micros();
+    unsigned long currentTimeMs = millis();
+
     static unsigned long startOfStateTime = 0;
     static unsigned long USlastReadTime = 0;
     static enum { starting, sendingPulse, waitingForPulseStart,
@@ -59,8 +81,8 @@ void loop() {
         break;
     case waitingForPulseEnd:
         if (!digitalRead(USpin)) {
-            pulsetime = currentTimeUs - startOfStateTime;
-            distance =  pulsetime / 58;
+            unsigned int pulsetime = currentTimeUs - startOfStateTime;
+            *distance =  pulsetime / 15;
             
             pinMode(USpin, OUTPUT);
             digitalWrite(USpin, LOW);
@@ -76,19 +98,4 @@ void loop() {
         }
         break;
     }
-
-    static unsigned long lastPrintTime = 0;
-    if (millis() - lastPrintTime >= 1000) {
-        //Serial.print("Time: ");   Serial.print(pulsetime); Serial.print('\t');
-        //Serial.print("Distance"); Serial.print(distance);  Serial.print('\t');
-        DEBUG_PRINT(pulsetime);
-        DEBUG_PRINT(distance);
-        DEBUG_PRINT(USstate);
-        DEBUG_PRINT(looptimeUs);
-        Serial.println();
-
-        lastPrintTime = millis();
-    }
-
-    lastTimeUs = currentTimeUs;
 }
