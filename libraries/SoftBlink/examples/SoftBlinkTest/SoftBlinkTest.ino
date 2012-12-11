96c4fbee40064bfc47f4cfcc6e58aa38bbe6bdc7
Adam Dunlap <dunlapadam@gmail.com>
2012-12-10 20:48:42 -0800

    Clearing up and adding comments to example

--- libraries/SoftBlink/examples/SoftBlinkTest/SoftBlinkTest.ino
+++ libraries/SoftBlink/examples/SoftBlinkTest/SoftBlinkTest.ino
@@ -5,10 +5,13 @@
 SoftBlink blueLED(9, 500);
 
 void setup() {
-    Serial.begin(115200);
+
+    // Set the pull-up resistor for a switch on pin 12
+    // We have to do this or the reading will give random values
     digitalWrite(12, HIGH);
 }
 
 void loop() {
-    blueLED.write(digitalRead(12));
+    bool switchVal = digitalRead(12);
+    blueLED.write(switchVal);
 }
